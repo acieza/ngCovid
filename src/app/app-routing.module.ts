14f937bd2c3577a5da797988b4075ca564cf88a3
Alejandro <asanjuancieza@gmail.com>
2020-11-24 11:45:23 +0100

    Inicio1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mundo', loadChildren: () => import('./pages/mundo/mundo.module').then(m => m.MundoModule) }, 
+  { path: 'espana', loadChildren: () => import('./pages/espana/espana.module').then(m => m.EspanaModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
